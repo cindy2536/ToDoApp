6a42234b1e152f5fb4e0bd538a2424437b250fca
cindy2536 <cai8862536@gmail.com>
2025-08-11 23:45:27 -0600

    Fix: use dynamic PORT for deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //Enable cors and json body parsing
 app.use(cors());
